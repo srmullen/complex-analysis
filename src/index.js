ec586ba73c4e19439f90a8aa12e5e182b2d73d71
Sean Mullen <srmullen13@gmail.com>
2017-11-25 11:07:09 -0700

    add gui params for C

--- src/index.js
+++ src/index.js
@@ -2,28 +2,41 @@ import 'tachyons';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import math from 'mathjs';
+import dat from 'dat.gui/build/dat.gui';
+import {partial} from 'lodash';
 
 window.math = math;
 
-const C = math.complex(0, 1);
-const R = math.eval(`(1 + sqrt(1 + 4 * ${vectorLength(C)})) / 2`);
-
+const params = {
+    x: 0,
+    y: 1
+}
 
 class Main extends Component {
     render () {
         return (
-            <canvas ref="canvas" width="500" height="500"></canvas>
+            <div>
+                <canvas ref="canvas" width="500" height="500"></canvas>
+            </div>
         );
     }
 
     componentDidMount () {
-        const ctx = this.refs.canvas.getContext('2d');
-        // const colors = [{r:255, a: 255}, {g: 255, a: 255}, {b: 255, a: 255}];
-        // colors[math.floor(math.random(0, 3))]
-        const imageData = ctx.getImageData(0, 0, 500, 500);
+        this.createGUI();
+        this.ctx = this.refs.canvas.getContext('2d');
+        // this.imageData = this.ctx.getImageData(0, 0, 500, 500);
+        this.drawJuliaSet();
+    }
+
+    drawJuliaSet () {
+        this.ctx.clearRect(0, 0, 500, 500);
+        const imageData = this.ctx.getImageData(0, 0, 500, 500);
+        const C = math.complex(params.x, params.y);
+        const R = math.eval(`(1 + sqrt(1 + 4 * ${vectorLength(C)})) / 2`);
+
         for (let x = 0; x < imageData.width; x++) {
             for (let y = 0; y < imageData.height; y++) {
-                const val = vectorLength(iterate(complexPolynomial, 10, createComplexFromGrid(x, y, {
+                const val = vectorLength(iterate(partial(complexPolynomial, C), 10, createComplexFromGrid(x, y, {
                     xOffset: imageData.width /2,
                     yOffset: imageData.height / 2
                 })))
@@ -35,7 +48,31 @@ class Main extends Component {
                 }
             }
         }
-        ctx.putImageData(imageData, 10, 10);
+        this.ctx.putImageData(imageData, 10, 10);
+    }
+
+    createGUI () {
+        const gui = new dat.GUI();
+        const cxController = gui.add(params, 'x');
+        const cyController = gui.add(params, 'y');
+        let previous = {
+            x: 0,
+            y: 1
+        }
+        cxController.onChange((x) => {
+            // console.log(params.x === x);
+            if (x != previous.x) {
+                previous.x = x;
+                this.drawJuliaSet();
+            }
+        });
+        cyController.onChange((y) => {
+            // console.log(params.x === x);
+            if (y != previous.y) {
+                previous.y = y;
+                this.drawJuliaSet();
+            }
+        });
     }
 }
 
@@ -65,7 +102,7 @@ function vectorLength (complex) {
     return complex.toPolar().r;
 }
 
-function complexPolynomial (z) {
+function complexPolynomial (C, z) {
     // f(z) = z^2
     return math.pow(z, 2).add(C);
 }
@@ -80,6 +117,6 @@ const iterate = (fn, times, z) => {
     return ret;
 }
 
-iterate(complexPolynomial, 5, math.complex(-1, 2));
+// iterate(complexPolynomial, 5, math.complex(-1, 2));
 
 ReactDOM.render(<Main />, document.getElementById('root'));
